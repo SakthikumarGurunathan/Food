a11fcda6b3db95e6821dff4786dfc0d1b38fc8d5
SakthikumarGurunathan <sakthikumargurunathan@gmail.com>
2023-09-06 23:02:21 +0530

    working on the restaurant menu page.

--- meshi/src/components/RestaurantMenu.jsx
+++ meshi/src/components/RestaurantMenu.jsx
@@ -1,22 +1,51 @@
+import './Body.css'
+import './style.css'
 import { useState,useEffect } from "react"
 import { useParams } from "react-router-dom"
 
 export default function RestaurantMenu(){
-    const [restaurantMenu,setRestaurantMenu] = useState({})
+    const [restaurantMenu,setRestaurantMenu] = useState([])
     const {id} = useParams()
-    console.log(id)
-
     // const [restaurantMenu,setRestaurantMenu] = useState()
     useEffect(()=>{
         fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=10.930515252798992&lng=76.9281879812479&restaurantId=${id}`)
         .then(res => res.json())
-        .then(data => setRestaurantMenu(data))
+        .then(data => setRestaurantMenu(data.data.cards))
     },[])
 console.log(restaurantMenu)
     return(
         <>
-            <h1>Restaurant Menu</h1>
-            <p>Id From url - <em><strong>{id}</strong></em></p>
+         {
+            restaurantMenu.length>0?
+        <div className='restaurant-whole-container'>
+            <div className="menu-container">
+                <div>
+                    <p className='hotel-name'>{restaurantMenu[0].card.card.info.name}</p>
+                    <p className='hotel-details'>{restaurantMenu[0].card.card.info.cuisines.join(', ')}</p>
+                    <p className='hotel-details'>{restaurantMenu[0].card.card.info.areaName}</p>
+                </div>
+                <div className='rating'>
+                    <div style={{display:"flex",justifyContent:"center",gap:"4px",alignItems:"center",paddingBottom:"10px",borderBottom: '1px solid #e9e9eb',marginBottom:"8px"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 512 512"><path fill="#3d9b6d" d="M496 203.3H312.36L256 32l-56.36 171.3H16l150.21 105.4l-58.5 171.3L256 373.84L404.29 480l-58.61-171.3Z"/></svg>
+                    <p style={{color: "#3d9b6d",fontWeight:"700"}}>{restaurantMenu[0].card.card.info.avgRating}</p>
+                    </div>
+                    <p style={{color: "#8b8d97",fontWeight:"600",fontSize:"11px"}}>{restaurantMenu[0].card.card.info.totalRatingsString}</p>
+                </div>
+            </div>
+            <hr style={{border: "none",borderBottom: "1px dashed #d3d3d3"}}/>
+            <div className='d-flex align-center gap-24'>
+                <div className='d-flex align-center time-cost-wrap gap-10'>
+                    <svg class="RestaurantTimeCost_icon__8UdT4" width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none"><circle r="8.35" transform="matrix(-1 0 0 1 9 9)" stroke="#3E4152" stroke-width="1.3"></circle><path d="M3 15.2569C4.58666 16.9484 6.81075 18 9.273 18C14.0928 18 18 13.9706 18 9C18 4.02944 14.0928 0 9.273 0C9.273 2.25 9.273 9 9.273 9C6.36399 12 5.63674 12.75 3 15.2569Z" fill="#3E4152"></path></svg>
+                    <span>{restaurantMenu[0].card.card.info.totalRatingsString}</span> 
+                </div>
+                <div className='d-flex align-center time-cost-wrap gap-10'>
+                    <svg class="RestaurantTimeCost_icon__8UdT4" width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none"><circle cx="9" cy="9" r="8.25" stroke="#3E4152" stroke-width="1.5"></circle><path d="M12.8748 4.495H5.6748V6.04H7.9698C8.7948 6.04 9.4248 6.43 9.6198 7.12H5.6748V8.125H9.6048C9.3798 8.8 8.7648 9.22 7.9698 9.22H5.6748V10.765H7.3098L9.5298 14.5H11.5548L9.1098 10.57C10.2048 10.39 11.2698 9.58 11.4498 8.125H12.8748V7.12H11.4348C11.3148 6.475 10.9698 5.905 10.4298 5.5H12.8748V4.495Z" fill="#3E4152"></path></svg>
+                    <span>{restaurantMenu[0].card.card.info.costForTwoMessage}</span>
+                </div>
+            </div>
+           </div> 
+            : <h1>Working</h1> 
+            }
         </>
     )
 }
