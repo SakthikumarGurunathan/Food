08f4955fd70ae2c0e49aa1d55ccd4f3b11add9fd
SakthikumarGurunathan <sakthikumargurunathan@gmail.com>
2023-09-07 22:50:46 +0530

    Added the offers,working on the menu items .

--- meshi/src/components/RestaurantMenu.jsx
+++ meshi/src/components/RestaurantMenu.jsx
@@ -12,7 +12,6 @@ export default function RestaurantMenu(){
         .then(res => res.json())
         .then(data => setRestaurantMenu(data.data.cards))
     },[])
-console.log(restaurantMenu)
     return(
         <>
          {
@@ -33,17 +32,63 @@ console.log(restaurantMenu)
                 </div>
             </div>
             <hr style={{border: "none",borderBottom: "1px dashed #d3d3d3"}}/>
-            <div className='d-flex align-center gap-24'>
+            <div className='d-flex align-center gap-24' style={{marginTop:"18px",marginBottom:"18px"}}>
                 <div className='d-flex align-center time-cost-wrap gap-10'>
-                    <svg class="RestaurantTimeCost_icon__8UdT4" width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none"><circle r="8.35" transform="matrix(-1 0 0 1 9 9)" stroke="#3E4152" stroke-width="1.3"></circle><path d="M3 15.2569C4.58666 16.9484 6.81075 18 9.273 18C14.0928 18 18 13.9706 18 9C18 4.02944 14.0928 0 9.273 0C9.273 2.25 9.273 9 9.273 9C6.36399 12 5.63674 12.75 3 15.2569Z" fill="#3E4152"></path></svg>
+                    <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none"><circle r="8.35" transform="matrix(-1 0 0 1 9 9)" stroke="#3E4152" stroke-width="1.3"></circle><path d="M3 15.2569C4.58666 16.9484 6.81075 18 9.273 18C14.0928 18 18 13.9706 18 9C18 4.02944 14.0928 0 9.273 0C9.273 2.25 9.273 9 9.273 9C6.36399 12 5.63674 12.75 3 15.2569Z" fill="#3E4152"></path></svg>
                     <span>{restaurantMenu[0].card.card.info.totalRatingsString}</span> 
                 </div>
                 <div className='d-flex align-center time-cost-wrap gap-10'>
-                    <svg class="RestaurantTimeCost_icon__8UdT4" width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none"><circle cx="9" cy="9" r="8.25" stroke="#3E4152" stroke-width="1.5"></circle><path d="M12.8748 4.495H5.6748V6.04H7.9698C8.7948 6.04 9.4248 6.43 9.6198 7.12H5.6748V8.125H9.6048C9.3798 8.8 8.7648 9.22 7.9698 9.22H5.6748V10.765H7.3098L9.5298 14.5H11.5548L9.1098 10.57C10.2048 10.39 11.2698 9.58 11.4498 8.125H12.8748V7.12H11.4348C11.3148 6.475 10.9698 5.905 10.4298 5.5H12.8748V4.495Z" fill="#3E4152"></path></svg>
+                    <svg  width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none"><circle cx="9" cy="9" r="8.25" stroke="#3E4152" stroke-width="1.5"></circle><path d="M12.8748 4.495H5.6748V6.04H7.9698C8.7948 6.04 9.4248 6.43 9.6198 7.12H5.6748V8.125H9.6048C9.3798 8.8 8.7648 9.22 7.9698 9.22H5.6748V10.765H7.3098L9.5298 14.5H11.5548L9.1098 10.57C10.2048 10.39 11.2698 9.58 11.4498 8.125H12.8748V7.12H11.4348C11.3148 6.475 10.9698 5.905 10.4298 5.5H12.8748V4.495Z" fill="#3E4152"></path></svg>
                     <span>{restaurantMenu[0].card.card.info.costForTwoMessage}</span>
                 </div>
             </div>
+            <div className='d-flex gap-10 offer-container-parent'>
+                {
+                restaurantMenu[1].card.card.gridElements.infoWithStyle.offers.map((each)=>{
+                    return ( 
+                    <div className='offer-container'>
+                    <div className='d-flex align-center gap-4'>
+                            <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${each.info.offerLogo}`} alt="" width={20} height={20}/>
+                            <p className='offer-header'>{each.info.header}</p>
+                        </div>
+                        <div className='d-flex align-center gap-4 ' style={{minWidth: "200px"}}>
+                            <p className='offer-desc-coup'>{each.info.couponCode}</p>
+                            <div className='desc-vertical-line offer-desc-coup'></div>
+                            <p className='offer-desc-coup' style={{marginTop:"3px",marginBottom:"-2px"}}>{each.info.description}</p>
+                        </div>
+                    </div>
+                    )
+                })
+                }
+                </div>
+                <div className='style-divider'></div>
+                <div className='main-dishes-container'>
+                    <div className='recommended'>
+                    
+                        {
+                        restaurantMenu[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.map((each)=>{
+                                return <div>
+                                    <p>{each?.card?.card?.title}</p>
+                                </div> 
+                            })
+                        } 
+                        {
+                            restaurantMenu[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.map((each)=>{
+                                const lengthVar = each?.card?.card?.itemCards
+                                console.log(lengthVar?.length,"without")
+                            })
+
+                        }
+                        {
+                            restaurantMenu[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.map((each)=>{
+                                const lengthVar = each?.card?.card?.categories
+                                console.log(lengthVar,"cat")
+                            }) 
+                        }
+                    </div>
+                </div>
            </div> 
+           
             : <h1>Working</h1> 
             }
         </>
